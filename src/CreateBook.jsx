835180a43888901ba248473e3f58ab1da79205be
Sirius-18 <97686497+Vignesh-Sivaraman@users.noreply.github.com>
2022-09-14 13:34:28 +0530

    task completed

---
+++ src/CreateBook.jsx
@@ -0,0 +1,124 @@
+import { useFormik } from "formik";
+import React, { useContext, useEffect } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import UserContext from "./UserContext";
+
+function CreateBook() {
+  let context = useContext(UserContext);
+  // useEffect(() => {
+  //   return loadData;
+  // });
+
+  // let loadData = async () => {
+  //   try {
+  //     let books = await axios.get(
+  //       "https://631f097322cefb1edc40d739.mockapi.io/books"
+  //     );
+  //     context.setbookcount(books.data.length);
+  //   } catch (error) {
+  //     console.error(error);
+  //   }
+  // };
+
+  const navigate = useNavigate();
+  const formik = useFormik({
+    initialValues: {
+      name: "",
+      bookid: "",
+      genre: "",
+      author: "",
+      year: "",
+    },
+    validate: (values) => {
+      let errors = {};
+      for (let keys in values) {
+        if (values[keys] === "") {
+          errors[keys] = `Please Enter ${keys}`;
+        }
+      }
+      return errors;
+    },
+    onSubmit: async (values) => {
+      await axios.post(
+        "https://631f097322cefb1edc40d739.mockapi.io/books",
+        values
+      );
+      alert("Book created");
+      navigate("/books");
+    },
+  });
+  return (
+    <div className="container">
+      <form onSubmit={formik.handleSubmit}>
+        <div className="row">
+          <div className="col-lg-6 mt-2">
+            <label>Book-Name</label>
+            <input
+              className="form-control"
+              type={"text"}
+              value={formik.values.name}
+              onChange={formik.handleChange}
+              name="name"
+            />
+            <span style={{ color: "red" }}>{formik.errors.name}</span>
+          </div>
+          <div className="col-lg-6 mt-2">
+            <label>Book-id</label>
+            <input
+              className="form-control"
+              type={"text"}
+              value={formik.values.bookid}
+              onChange={formik.handleChange}
+              name="bookid"
+            />
+            <span style={{ color: "red" }}>{formik.errors.bookid}</span>
+          </div>
+          <div className="col-lg-6 mt-2">
+            <label>Genre</label>
+            <input
+              className="form-control"
+              type={"text"}
+              value={formik.values.genre}
+              onChange={formik.handleChange}
+              name="genre"
+            />
+            <span style={{ color: "red" }}>{formik.errors.genre}</span>
+          </div>
+          <div className="col-lg-6  mt-2">
+            <label>Author</label>
+            <input
+              className="form-control"
+              type={"text"}
+              value={formik.values.author}
+              onChange={formik.handleChange}
+              name="author"
+            />
+            <span style={{ color: "red" }}>{formik.errors.author}</span>
+          </div>
+          <div className="col-lg-6 mt-2">
+            <label>Published-year</label>
+            <input
+              className="form-control"
+              type={"text"}
+              value={formik.values.year}
+              onChange={formik.handleChange}
+              name="year"
+            />
+            <span style={{ color: "red" }}>{formik.errors.year}</span>
+          </div>
+
+          <div className="col-lg-6 mt-2">
+            <input
+              className=" btn btn-primary"
+              type={"submit"}
+              value="Submit"
+            />
+          </div>
+        </div>
+      </form>
+    </div>
+  );
+}
+
+export default CreateBook;
